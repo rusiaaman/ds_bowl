4541922bacf973d1433af1aaf53b1eb25481e8c2
Aman Rusia <rusia.aman@gmail.com>
2018-04-08 13:27:02 +0530

    Dropouts added everywhere

--- src/unet_bn_dropout.py
+++ src/unet_bn_dropout.py
@@ -5,29 +5,34 @@ def get_model():
     Input_layer = Input(shape=(360,360,3))
     activation='relu'
     c1 = Conv2D(32, (3, 3), strides=1, activation=activation, kernel_initializer='he_normal', padding='same') (Input_layer)
+    c1 = Dropout(0.01)(c1)
     c1 = Conv2D(32, (3, 3), strides=1, activation=activation, kernel_initializer='he_normal', padding='same') (c1)
     p1 = MaxPooling2D((2, 2)) (c1)
     
     c2 = BatchNormalization()(p1)
     c2 = Conv2D(64, (3, 3), strides=1,activation=activation, kernel_initializer='he_normal', padding='same') (c2)
+    c2 = Dropout(0.02)(c2)
     c2 = Conv2D(64, (3, 3), strides=1,activation=activation, kernel_initializer='he_normal', padding='same') (c2)
     c2 = Conv2D(64, (3, 3), strides=1,activation=activation, kernel_initializer='he_normal', padding='same') (c2)
     p2 = MaxPooling2D((2, 2)) (c2)
 
     c3 = BatchNormalization()(p2)
     c3 = Conv2D(128, (3, 3), strides=1,activation=activation, kernel_initializer='he_normal', padding='same') (c3)
+    c3 = Dropout(0.05) (c3)
+    c3 = Conv2D(128, (3, 3), strides=1,activation=activation, kernel_initializer='he_normal', padding='same') (c3)
     c3 = Conv2D(128, (3, 3), strides=1,activation=activation, kernel_initializer='he_normal', padding='same') (c3)
     p3 = MaxPooling2D((2, 2)) (c3)
 
     c4 = BatchNormalization()(p3)
     c4 = Conv2D(256, (3, 3), strides=1,activation=activation, kernel_initializer='he_normal', padding='same') (c4)
+    c4 = Dropout(0.05)(c4)
     c4 = Conv2D(256, (3, 3), strides=1,activation=activation, kernel_initializer='he_normal', padding='same') (c4)
     c4 = Conv2D(256, (3, 3), strides=1,activation=activation, kernel_initializer='he_normal', padding='same') (c4)
     p4 = MaxPooling2D((2, 2)) (c4)
 
     c5 = BatchNormalization()(p4)
     c5 = Conv2D(512, (3, 3), strides=1,activation=activation, kernel_initializer='he_normal', padding='same') (c5)
-    c5 = Dropout(0.2)(c5)
+    c5 = Dropout(0.07)(c5)
     c5 = Conv2D(512, (3, 3), strides=1,activation=activation, kernel_initializer='he_normal', padding='same') (c5)
     c5 = Conv2D(512, (3, 3), strides=1,activation=activation, kernel_initializer='he_normal', padding='same') (c5)
     p5 = MaxPooling2D((2, 2)) (c5)
@@ -63,7 +68,7 @@ def get_model():
     u8 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c7)
     u8 = BatchNormalization()(u8)
     u8 = concatenate([u8, c2])
-    c8 = Conv2D(64, (3, 3), strides=1,activation=activation, kernel_initializer='he_normal', padding='same') (u8)
+    c8 = Conv2D(64, (3, 3), strides=1,activation=activation, kernel_initializer='he_normal', padding='same') (u8)    
     c8 = Dropout(0.1)(c8)
     c8 = Conv2D(64, (3, 3), strides=1,activation=activation, kernel_initializer='he_normal', padding='same') (c8)
     c8 = Conv2D(64, (3, 3), strides=1,activation=activation, kernel_initializer='he_normal', padding='same') (c8)
@@ -72,12 +77,10 @@ def get_model():
     u9 = BatchNormalization()(u9)
     u9 = concatenate([u9, c1], axis=3)
     c9 = Conv2D(32, (3, 3), strides=1,activation=activation, kernel_initializer='he_normal', padding='same') (u9)
-    c9 = Dropout(0.1)(c9)
     c9 = Conv2D(32, (3, 3), strides=1,activation=activation, kernel_initializer='he_normal', padding='same') (c9)
-
+    
     output_layer = Conv2D(1, (1, 1),strides=1, activation='sigmoid') (c9)
 
     model = Model(inputs=[Input_layer], outputs=[output_layer])
     return model
 best_model_fname=None
-#best_model_fname='weights_bn.13-0.05.hdf5'
